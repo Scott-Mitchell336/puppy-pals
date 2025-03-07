8d4b81b44247b7eb9254d2e02281e885112404d7
BitBlasters <scottmitchell@mac.com>
2025-03-06 21:36:18 -0700

    Now showing puppy names on the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-
+import { useState } from 'react';
 import './App.css'
+import puppyList from './data.js'
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
 
+  console.log("App() - puppies = ", puppies);
   return (
-    <>
+    <div className="App">
+      {
+        puppies.map((puppy) =>{
+          return <p key={puppy.id}>{puppy.name}</p>
+        })
+      }
       
-    </>
+    </div>
   )
 }
 
