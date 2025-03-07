f799ba85d5ab9bb3a135d8d54ad8415169120077
BitBlasters <scottmitchell@mac.com>
2025-03-06 22:07:35 -0700

    Shows puppy names, age and email

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,37 @@ import puppyList from './data.js'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setfeatPupId] = useState(null);
 
   console.log("App() - puppies = ", puppies);
+
+  function handleClick(){
+    // some logic here
+  }
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log("App() - featuredPup = ", featuredPup);
+
   return (
     <div className="App">
       {
         puppies.map((puppy) =>{
-          return <p key={puppy.id}>{puppy.name}</p>
+          return <p onClick={() =>{
+            setfeatPupId(puppy.id);
+          }} key={puppy.id}>{puppy.name}</p>
         })
       }
-      
+      { featPupId && (
+        <div>
+        <h2>{featuredPup.name}</h2>
+        <ul>
+          <li>Age: {featuredPup.age}</li>
+          <li>Email: {featuredPup.email}</li>
+        </ul>
+        </div>
+        )}
     </div>
+    
   )
 }
 
