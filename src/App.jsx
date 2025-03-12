8e9c29e327d0ec1461687190bfa104bc836c740b
BitBlasters <scottmitchell@mac.com>
2025-03-11 18:54:57 -0600

    Commiting for turning in for a grade.

--- src/App.jsx
+++ src/App.jsx
@@ -8,33 +8,30 @@ function App() {
 
   console.log("App() - puppies = ", puppies);
 
-  function handleClick(){
-    // some logic here
-  }
-
   const featuredPup = puppies.find((pup) => pup.id === featPupId);
   console.log("App() - featuredPup = ", featuredPup);
 
   return (
     <div className="App">
-      {
-        puppies.map((puppy) =>{
-          return <p onClick={() =>{
-            setfeatPupId(puppy.id);
-          }} key={puppy.id}>{puppy.name}</p>
-        })
-      }
+      <div className="puppy-list">
+        {
+          puppies.map((puppy) =>{
+            return <p onClick={() =>{
+              setfeatPupId(puppy.id);
+            }} key={puppy.id}>{puppy.name}</p>
+          })
+        }
+      </div>
       { featPupId && (
-        <div>
-        <h2>{featuredPup.name}</h2>
-        <ul>
-          <li>Age: {featuredPup.age}</li>
-          <li>Email: {featuredPup.email}</li>
-        </ul>
+        <div className="featured-pup">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
         </div>
-        )}
+      )}
     </div>
-    
   )
 }
 
